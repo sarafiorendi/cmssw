1d54f61278c673ba6124d766ae3ed94266f3c8d1
sara <sara.fiorendi@cern.ch>
2024-08-27 16:58:21 +0200

    add code to dump cluster info

---
+++ EventFilter/Phase2TrackerRawToDigi/plugins/Phase2TrackerDumpDigi.cc
@@ -0,0 +1,119 @@
+#include "CondFormats/SiStripObjects/interface/Phase2TrackerCabling.h"
+#include "CondFormats/DataRecord/interface/Phase2TrackerCablingRcd.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/DetId/interface/DetIdCollection.h"
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/Phase2TrackerCluster/interface/Phase2TrackerCluster1D.h"
+#include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
+#include "DataFormats/Common/interface/DetSet.h"
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/FEDRawData/interface/FEDNumbering.h"
+#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerDigi.h"
+#include "DataFormats/L1TrackTrigger/interface/TTTypes.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDHeader.h"
+#include "EventFilter/Phase2TrackerRawToDigi/interface/utils.h"
+#include "Geometry/CommonDetUnit/interface/PixelGeomDetUnit.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/Records/interface/IdealGeometryRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include <sstream>
+
+using namespace std;
+using namespace Phase2Tracker;
+
+
+class Phase2TrackerDumpDigi : public edm::one::EDAnalyzer<edm::one::WatchRuns> {
+public:
+  /// constructor
+  Phase2TrackerDumpDigi(const edm::ParameterSet& pset);
+  /// default constructor
+  ~Phase2TrackerDumpDigi() override = default;
+  void beginRun(edm::Run const&, edm::EventSetup const&) override;
+  void endRun(edm::Run const& iEvent, edm::EventSetup const&) override {};
+  void analyze(const edm::Event&, const edm::EventSetup&);
+
+private:
+  const edm::ESGetToken<Phase2TrackerCabling, Phase2TrackerCablingRcd> ph2CablingESToken_;
+  const edm::ESGetToken<TrackerGeometry, TrackerDigiGeometryRecord> geomToken_;
+  const edm::ESGetToken<TrackerTopology, TrackerTopologyRcd> topoToken_;
+  const edm::EDGetTokenT<Phase2TrackerCluster1DCollectionNew> token_;
+  const Phase2TrackerCabling* cabling_ = nullptr;
+  const TrackerTopology* tTopo_ = nullptr;
+  const TrackerGeometry* tGeom_ = nullptr;
+  std::map<int, std::pair<int, int>> stackMap_;
+};
+
+Phase2TrackerDumpDigi::Phase2TrackerDumpDigi(const edm::ParameterSet& pset)
+    : ph2CablingESToken_(esConsumes<Phase2TrackerCabling, Phase2TrackerCablingRcd, edm::Transition::BeginRun>()),
+      geomToken_(esConsumes<TrackerGeometry, TrackerDigiGeometryRecord, edm::Transition::BeginRun>()),
+      topoToken_(esConsumes<TrackerTopology, TrackerTopologyRcd, edm::Transition::BeginRun>()),
+      token_(
+          consumes<Phase2TrackerCluster1DCollectionNew>(pset.getParameter<edm::InputTag>("ProductLabel"))) {
+}
+
+void Phase2TrackerDumpDigi::beginRun(edm::Run const& run, edm::EventSetup const& es) {
+    cabling_ = &es.getData(ph2CablingESToken_);
+    tGeom_ = &es.getData(geomToken_);
+    tTopo_ = &es.getData(topoToken_);
+
+    // build map of upper and lower for each module
+//     for (auto iu = tGeom_->detUnits().begin(); iu != tGeom_->detUnits().end(); ++iu) {
+//       unsigned int detId_raw = (*iu)->geographicalId().rawId();
+//       DetId detId = DetId(detId_raw);
+//       if (detId.det() == DetId::Detector::Tracker) {
+//         if (tTopo_->isLower(detId) != 0) {
+//           stackMap_[tTopo_->stack(detId)].first = detId;
+//         }
+//         if (tTopo_->isUpper(detId) != 0) {
+//           stackMap_[tTopo_->stack(detId)].second = detId;
+//         }
+//       }
+//     }  // end loop on detunits
+  }
+
+void Phase2TrackerDumpDigi::analyze(const edm::Event& event, const edm::EventSetup& es) {
+//     std::unique_ptr<FEDRawDataCollection> buffers(new FEDRawDataCollection);
+  edm::Handle<Phase2TrackerCluster1DCollectionNew> clusters_handle;
+  event.getByToken(token_, clusters_handle);
+  std::cout << "size of clusters: " << clusters_handle.product()->size() <<  std::endl;
+  for (const auto& DSVItr : *clusters_handle) {
+    std::cout << "inside loop" << std::endl;
+    // Getting the id of detector unit
+    uint32_t rawid(DSVItr.detId());
+    DetId detId(rawid);
+    const GeomDetUnit* geomDetUnit(tGeom_->idToDetUnit(detId));
+    if (!geomDetUnit)
+      continue;
+    for (const auto& clusterItr : DSVItr) { 
+    
+      // temporary restriction to 2S modules
+//       TrackerGeometry::ModuleType mType = tGeom_->getDetectorType(detId);
+//       if (mType != TrackerGeometry::ModuleType::Ph2SS)
+//         continue;
+      
+      std::cout << "detId: " << detId.rawId() << std::endl;
+      // I could find this number in the DetId sensors list e.g. https://cms-tklayout.web.cern.ch/cms-tklayout/layouts/repository-git-dev/OT616_200_IT404/index.html
+      MeasurementPoint mpCluster(clusterItr.center(), clusterItr.column() + 0.5);
+      Local3DPoint localPosCluster = geomDetUnit->topology().localPosition(mpCluster);
+      Global3DPoint globalPosCluster = geomDetUnit->surface().toGlobal(localPosCluster);
+      std::cout << "\t cluster r position: " << globalPosCluster.perp()  << std::endl; 
+      std::cout << "\t cluster global z position: " << globalPosCluster.z()  << std::endl; 
+    }  
+  }      
+
+}
+
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE(Phase2TrackerDumpDigi);
---
+++ EventFilter/Phase2TrackerRawToDigi/test/Phase2TrackerDumpDigi_cfg.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+import sys
+
+process = cms.Process("DumpDigi")
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+# Input source
+# process.source = cms.Source("PoolSource",
+#     fileNames = cms.untracked.vstring("/store/relval/CMSSW_14_0_0_pre2/RelValDisplacedSingleMuFlatPt1p5To8/GEN-SIM-DIGI-RAW/133X_mcRun4_realistic_v1_STD_2026D98_noPU_RV229-v1/2580000/3ce31040-55a5-4469-8ee2-16d050bb6ade.root")
+#  )
+
+process.load('P2TrackerCabling_cfi')
+process.Phase2TrackerDumpDigi = cms.EDAnalyzer(
+    'Phase2TrackerDumpDigi',
+    ProductLabel = cms.InputTag("siPhase2Clusters")
+)
+
+
+### test our digis after digi-raw-digi ###
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring("file:rawtodigi.root")    
+)
+process.Phase2TrackerDumpDigi.ProductLabel = cms.InputTag("Phase2TrackerDigiProducer", "Sparsified", "RawToDigi")
+### end test our digis after digi-raw-digi ###
+
+
+## Geometry D98
+## from https://github.com/cms-sw/cmssw/blob/master/Configuration/Geometry/python/dict2026Geometry.py
+##    ("O9","T32","C17","M10","F8","I16") : "D98",
+## https://github.com/cms-sw/cmssw/blob/master/Configuration/Geometry/python/dict2026Geometry.py#L221C14-L221C86
+process.load('Configuration.Geometry.GeometryExtended2026D98Reco_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+# Temporary change until we switch to D110 geometry.
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '140X_mcRun4_realistic_v3', '')
+process.p = cms.EndPath(process.Phase2TrackerDumpDigi)
